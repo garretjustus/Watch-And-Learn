6a7c08d9c67ea652a7ebaa56aa8a55cd35ae3315
garret <garretcabello@gmail.com>
2020-08-24 09:04:40 -0400

    start of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResultsPageComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
