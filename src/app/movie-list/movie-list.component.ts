9b7ae650e675ccbb53b0de7835817a29bf134aa3
kwisniowiecki <kristi.wisniowiecki@gmail.com>
2020-08-25 10:25:06 -0400

    added movie posters, title, and year

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
 })
 export class MovieListComponent implements OnInit {
+  @Input() movieRef: any;
 
-  constructor() { }
+  baseUrl: string = 'https://image.tmdb.org/t/p/w154';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
